cbb68ebcef9f7291e6755a0d14def49008372bfa
TheUltimateOptimist <digitalConfidence@web.de>
2025-09-15 13:17:50 +0200

    changes

--- auth/main.py
+++ auth/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, Response
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -8,4 +9,8 @@ async def auth(request: Request):
     Simple auth endpoint for Nginx auth_request.
     Always returns 200 OK.
     """
-    return Response(status_code=200)
+    return RedirectResponse(url="https://www.google.com", status_code=302)
+    # not authenticated:
+    return Response(status_code=401)
+    # not authorised:
+    return Response(status_code=403)
